783d160677e48d8d343831b4f05526f24f956ccb
byah24@outlook.com <lab53@fatec.com>
2025-03-24 09:55:18 -0300

    Exercícios desenvolvidos em aula, repositório usado para anotação.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+//double numero01, numero02, resultado;
+//
+//Console.WriteLine("Digite os números que você quer!");
+//numero01 = Convert.ToDouble(Console.ReadLine());
+//numero02 = Convert.ToDouble(Console.ReadLine());
+//
+//resultado = numero01 / numero02;
+//
+//Console.WriteLine($"O resultado é: {resultado:N15}");
+
+//pi * r^2
+
+const double valorPiAproximado = 3.1416;
+double raio, areaCirculo;
+
+Console.WriteLine("-------Área do Círculo-------");
+Console.WriteLine("Digite o raio do seu círculo:");
+raio = Convert.ToDouble(Console.ReadLine());
+
+areaCirculo = Math.PI * Math.Pow(raio,2);
+
+Console.WriteLine($"A área do círculo é: {areaCirculo}");
+
